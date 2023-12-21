524b9080c44e6517a82d302bb2eb3d0931e0194b
Evan Richter <evanjrichter@gmail.com>
2023-12-21 15:27:18 -0700

    trim summary whitespace

--- src/bin/details.rs
+++ src/bin/details.rs
@@ -5,13 +5,17 @@ fn main() -> Result<(), Box<dyn Error>> {
     let mut lines = stdin.lines();
 
     let first = lines.next().ok_or("no input")??;
+    let first = first.trim();
     if first.starts_with("<details>") {
-        return Err("undo details not supported yet".into());
+        Err("undo details not supported yet".into())
+    } else {
+        make_details(lines, first).map_err(Into::into)
     }
+}
 
-    // make the first line the summary text
+fn make_details<E>(lines: impl Iterator<Item = Result<String, E>>, summary: &str) -> Result<(), E> {
     println!("<details>");
-    println!("  <summary>{first}</summary>");
+    println!("  <summary>{summary}</summary>");
     println!();
 
     // the rest of the input is body
