16e17d444fc1a96135350dd820f56e8f76143957
Evan Richter <evanjrichter@gmail.com>
2023-12-21 09:03:25 -0700

    add linux support

--- flake.nix
+++ flake.nix
@@ -2,6 +2,6 @@
   inputs.flakelight-rust.url = "github:accelbread/flakelight-rust";
   outputs = { flakelight-rust, ... }:
     flakelight-rust ./. {
-      systems = [ "aarch64-darwin" ];
+      systems = [ "aarch64-darwin" "x86_64-linux" ];
     };
 }
